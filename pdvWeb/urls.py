be423b0cb7be3ca84f4c7874322c1c22059ed187
EmersonMessiasDev <emerson.almeida@a.ficr.edu.br>
2023-04-19 17:03:34 -0300

    PDV-WEB_DJANGO

---
+++ pdvWeb/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+
+app_name = 'pdvWeb'
+
+urlpatterns = [
+    # path('', views.progresso, name='progresso'),
+    
+]
