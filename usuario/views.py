5c87560f0ddfcc85a4ec02e908afffd7b80a5b28
GilzinCode <88803588+GilzinCode@users.noreply.github.com>
2023-05-11 21:20:52 -0300

    templatesv2

--- usuario/views.py
+++ usuario/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 def cadastro(request):
   return render(request, 'cadastro.html')
 
+def funcionarios(request):
+  return render(request, 'funcionarios.html')
+
 def login(request):
   return render(request, 'login.html')
 
